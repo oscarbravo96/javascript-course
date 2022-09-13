84399f74ef4b6ad7b47196ae2e06592935caf49d
oscarbravo96 <96oscarbravo@gmail.com>
2022-09-13 14:35:17 -0400

    finished modal and pig game projects

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -6,5 +6,26 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
